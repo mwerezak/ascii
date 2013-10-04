62840f9c846c4292eca0931281a65d078b7969e5
Mike <mwerezak@gmail.com>
2013-10-04 00:50:21 -0400

    Added non-breaking space support to Text widget

--- textwidgets.py
+++ textwidgets.py
@@ -18,16 +18,17 @@ class Label(object):
             self.style.apply(canvas)
             canvas.printstr(0,0,self.text)
 
-#TODO: non-breaking space?
 class Text (object):
     """ A widget that displays a reflowable column of text. Always starts a new line at newline characters.
+        If nbsp is a character, then that character will be used as non-breaking space.
     """
-    def __init__(self, text, max_width, text_align="left", **style):
+    def __init__(self, text, max_width, text_align="left", nbsp=None, **style):
         self.style = CanvasStyle(style)
         if max_width <= 0: raise ValueError("max_width must be greater than 0")
         
         self.max_width = max_width
         self.text_align = text_align
+        self.nbsp = nbsp
         self._lines = []    #A list of Label objects - one for each line
         
         self.text = text
@@ -41,6 +42,16 @@ class Text (object):
         self._text = value
         self._lines = [Label(text) for text in self._get_line_text()] #generate lines from text flow
     
+    def _is_space(self, c):
+        """ Returns True if c should be considered whitespace. """
+        if c == self.nbsp:
+            return False
+        return c.isspace()
+        
+    def _is_newline(self, c):
+        """ Returns True if c should be considered to start a new line. """
+        return c == "\n"
+    
     def _split(self, text):
         """ Returns an iterator that breaks text into chunks consisting of a continuous block of non-whitespace followed by a
             continouse block of whitespace.
@@ -48,19 +59,20 @@ class Text (object):
         if len(text) == 0:
             return
             
-        last_ws = False     #was the last character whitespace?
-        i = 0               #beginning of chunk index
+        is_last_space = False   #was the last character whitespace?
+        i = 0                   #beginning of chunk index
         for j in range(len(text)):
-            if (text[j].isspace() and not last_ws) or text[j] == "\n":
+            is_space = self._is_space(text[j])
+            if (is_space and not is_last_space) or self._is_newline(text[j]):
                 yield text[i:j]
                 i = j       #begin a new chunk
-            last_ws = text[j].isspace()
+            is_last_space = is_space
         yield text[i:]
     
     def _get_line_text(self):
         line = ""
         for chunk in self._split(self.text):
-            if len(chunk) > 0 and chunk[0] == "\n":         #break on newlines
+            if len(chunk) > 0 and self._is_newline(chunk[0]):         #break on newlines
                 yield line
                 line = chunk.lstrip()
             elif len(line) + len(chunk) > self.max_width:   #break if the next chunk does not fit
@@ -99,7 +111,7 @@ if __name__ == "__main__":
     
     console.init(30,30,"Text Test")
     
-    text = Text('The standard chunk of  \t\n LoremIpsumusedsincethe1500sisreproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also repr\noduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 15, bg_colour=dlib.darker_red)
+    text = Text('The standard chunk of \n   LoremIpsumusedsincethe1500sisreproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also repr\noduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.', 15, bg_colour=dlib.darker_red)   #, nbsp=' ')
     
     (text >> Border(double_line)).render(console.canvas(), 2, 2)
     console.flush()
